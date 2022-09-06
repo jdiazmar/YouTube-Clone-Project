27b8f167b983b29c5e7c6d71e6b49d2828dd8b18
Josmar Diaz <jdm.begreat28@gmail.com>
2022-09-06 18:35:30 -0400

    Renamed Footer In App

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright © 2022</p>{" "}
+      <p>dCC YouTube Clone Project © 2022</p>{" "}
     </footer>
   );
 };
