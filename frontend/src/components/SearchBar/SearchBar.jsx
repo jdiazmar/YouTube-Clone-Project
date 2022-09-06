500d8b3ad1ca4299c000d3df19643ac4ab7d8eed
Josmar Diaz <jdm.begreat28@gmail.com>
2022-09-06 17:47:30 -0400

    Added SearchBar Functionallity In SearchBar Component

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+
+const SearchBar = (props) => {
+
+const [searchVideo, setSearchVideo] = useState('');
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(searchVideo);
+    props.getSearchVideo(searchVideo);
+    setSearchVideo('');
+}
+
+    return ( 
+        <div>
+        <form  onSubmit={handleSubmit}>
+            <label >Search:</label>
+            <input type='text' placeholder="Search for a video..." className='search-input' value={searchRequest} onChange ={(event) => setSearchRequest(event.target.value)} />
+            <button>Search</button>
+        </form>
+    </div>
+    );
+}
+ 
+export default SearchBar;
