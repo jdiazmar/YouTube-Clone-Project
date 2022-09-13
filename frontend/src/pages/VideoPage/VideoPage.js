41a86e05bde57581745c2d2431703026b8161867
Josmar Diaz <jdm.begreat28@gmail.com>
2022-09-13 16:43:48 -0400

    Created Funcitonallity for VideoPage

---
+++ frontend/src/pages/VideoPage/VideoPage.js
@@ -0,0 +1,48 @@
+// Generic Imports
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { KEY } from '../../localKey';
+// Component Imports
+import SearchBar from '../../components/SearchBar/SearchBar';
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
+
+const VideoPage = (props) => {
+
+    const [searchResults, setSearchResults] = useState('');
+    const [videoId, setVideoId] = useState('');
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    useEffect(() => {
+        getSearchResults();
+    }, [])
+
+    async function getSearchResults(searchTerm='nba 2k23'){
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${KEY}`);
+        setVideoId(response.data.items[0].id.videoId);
+        setTitle(response.data.items[0].snippet.title);
+        setDescription(response.data.items[0].snippet.description);
+        setSearchResults(response.data.items);
+        console.log(response.data.items);
+    }
+    }
+    
+
+    return ( 
+        <div>
+            <div> <Link to='/register'> <b>Register Here!</b></Link> </div>
+            <div> <SearchBar getSearchResults={getSearchResults} /> </div>
+            <div>
+                <div>
+                    <VideoPlayer videoId={videoId} title={title} description={description}  />
+                </div>
+            </div>
+            <div>
+                <Link to='/login'><button>Login Here</button></Link>
+            </div>
+        </div>
+     );
+
+ 
+export default VideoPage;
