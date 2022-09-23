605658b122dd693cc42d12d2f9c6ba6d84c90a4d
Josmar Diaz <jdm.begreat28@gmail.com>
2022-09-23 16:18:16 -0400

    Added SearchResultsPage to App.js and Linked it  + Adding Functionallity to SearchResultsPage Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import YouTubePage from "./pages/YouTubePage/YouTubePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import VideoPage from "./pages/VideoPage/VideoPage";
+import SearchResultsPage from "./pages/SearchResultsPage/SearchResultsPage";
 
 // Component Imports
 import Navbar from "./components/NavBar/NavBar";
@@ -31,6 +32,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path='/videos' element={<VideoPage />} />
+        <Route path='/search' element={<SearchResultsPage />} />
       </Routes>
       <Footer />
     </div>
---
+++ frontend/src/pages/SearchResultsPage/SearchResultsPage.js
@@ -0,0 +1,42 @@
+// Generic Imports
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { KEY } from '../../localKey';
+// Component Imports
+import SearchBar from '../../components/SearchBar/SearchBar';
+import { Link } from 'react-router-dom';
+
+
+const SearchResultsPage = (props) => {
+
+    const [searchResults, setSearchResults] = useState([]);
+const [videoId, setVideoId] = useState('');
+const [description, setDescription] = useState('');
+const [title, setTitle] = useState('');
+
+useEffect(() => {
+    getSearchResults();
+}, [])
+
+async function getSearchResults(searchTerm = 'nba 2k23'){
+    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${searchTerm}&key=${KEY}&part=snippet`);
+    setSearchResults(response.data.items)
+}
+
+    
+    
+    return ( 
+        <div>
+            <div>
+                <Link to='/register'> <b>Register Here!</b> </Link>
+            </div>
+            <div>
+                <h2>Search Page</h2>
+            </div>
+            <div> <SearchBar getSearchResults={getSearchResults} /> </div>
+            <div>    </div>
+        </div>
+     );
+}
+ 
+export default SearchResultsPage;
